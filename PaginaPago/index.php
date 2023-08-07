1d74740f54b1bf5f7e077509004731bea602e14a
Paco <paco.refractando@gmail.com>
2023-08-07 12:56:35 +0200

    ligeros cambios

--- PaginaPago/index.php
+++ PaginaPago/index.php
@@ -22,7 +22,7 @@ $listaProductos=$productos->fetchAll(PDO::FETCH_OBJ);
 
     <nav class="navbar navbar-expand-lg navbar-light bg-dark" >
     <div class="container" style="margin-top: none">
-        <a class="navbar-brand  text-white" href="#">Pay Page</a>
+        <a class="navbar-brand  text-white" href="#">¡Compra tu disfraz!</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
@@ -40,9 +40,9 @@ $listaProductos=$productos->fetchAll(PDO::FETCH_OBJ);
                     <img height="213px" class="card-img-top" src="images/<?php echo $producto->imagen;?>">
                     <div class="card-body" >
                         <h5 class="d-inline"><b><?php echo $producto->nombre;?></b> </h5>
-                        <h5 class="d-inline"><div class="text-muted d-inline"><?php echo $producto->precio;?></div></h5>
+                        <h5 class="d-inline"><div class="text-muted d-inline"><?php echo $producto->precio;?> €</div></h5>
                         <p><?php echo $producto->descripcion;?> </p>
-                         <a href="#"  class="btn btn-primary w-100 rounded my-2"> Pay Now <i class="fas fa-arrow-right"></i> </a>      
+                         <a href="#"  class="btn btn-primary w-100 rounded my-2"> Comprar <i class="fas fa-arrow-right"></i> </a>      
      
                     </div>
                 </div>
