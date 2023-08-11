ac4679e4fda79dee35fc9f729dcf014678923c69
Paco <paco.refractando@gmail.com>
2023-08-11 10:12:30 +0200

    corregido bug en barra de navegacion

--- PaginaPago/index.php
+++ PaginaPago/index.php
@@ -14,7 +14,7 @@ $listaProductos=$productos->fetchAll(PDO::FETCH_OBJ);
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://kit.fontawesome.com/5c5946fe44.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <title>Tienda de Disfraces</title>
@@ -23,19 +23,15 @@ $listaProductos=$productos->fetchAll(PDO::FETCH_OBJ);
     <!-- Barra de navegacion -->
     <nav class="navbar navbar-dark navbar-expand-lg bg-primary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#" >Compra tu disfraz!</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse d-flex flex-row-reverse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link active" href="#contacto">Contacto</a>
-        </li>
-      </ul>
+    <div class="collapse navbar-collapse flex-row-reverse" id="navbar">
+      <div class="navbar-nav">
+        <a class="nav-link active" aria-current="page" href="#">Home</a>
+        <a class="nav-link active" href="#contacto">Contacto</a>
+      </div>
     </div>
   </div>
 </nav>
@@ -119,8 +115,8 @@ $listaProductos=$productos->fetchAll(PDO::FETCH_OBJ);
             
         </div>
         </footer>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="" crossorigin="anonymous"></script>
+        <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="" crossorigin="anonymous"></script> -->
+        <script src="	https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="" crossorigin="anonymous"></script>
   </body>
  
 </html>
